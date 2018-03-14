a83b0832b07c879c1865c0443d9f1999f883082e
PreciousDiaz <elladiaz5@gmail.com>
2018-03-14 13:31:50 +0800

    Modify the Timeline, Top-nav, and Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyB5Td6lEcRZ9Ag1SYJBy9kS0Ku9d7KIq1Y",
+    authDomain: "angularsocial-aa5c2.firebaseapp.com",
+    databaseURL: "https://angularsocial-aa5c2.firebaseio.com",
+    projectId: "angularsocial-aa5c2",
+    storageBucket: "angularsocial-aa5c2.appspot.com",
+    messagingSenderId: "40474942095"
     }
 };
